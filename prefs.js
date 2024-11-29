930aeb32472b8f77a9b16fe2f5179264001ed77c
Nicolas <nicolas@noisyspoon.net>
2024-11-29 23:04:24 +0100

    Traduction

--- prefs.js
+++
@@ -1,8 +0,0 @@
-import { ExtensionPreferences } from 'resource:///org/gnome/Shell/Extensions/js/extensions/prefs.js';
-
-
-export default class ExamplePreferences extends ExtensionPreferences {
-  fillPreferencesWindow(window) {
-    window._settings = this.getSettings('org.gnome.shell.extensions.rd-ssh-connect');
-  }
-}
